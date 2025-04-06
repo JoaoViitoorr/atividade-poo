f9fdf2b8e138bfb4d92291bac42db5c9631976bf
JoaoViitoorr <joaovitoroliveirasimao9@gmail.com>
2025-04-06 19:17:43 -0300

    quinto commit branch05

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
 public class Main {
+    public static double media(int[] numeros) {
+        int soma = 0;
+        for (int num : numeros) {
+            soma += num;
+        }
+        return (double) soma / numeros.length;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println(media(new int[]{1, 2, 3, 4, 5})); // 3.0
     }
 }
