98736c4900d5624fa8cc454961f45fdd5dae0b2e
JoaoViitoorr <joaovitoroliveirasimao9@gmail.com>
2025-04-06 19:20:16 -0300

    sexto commit branch06

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,14 @@
 public class Main {
+    public static int contarVogais(String str) {
+        int count = 0;
+        for (char c : str.toLowerCase().toCharArray()) {
+            if ("aeiou".indexOf(c) != -1) {
+                count++;
+            }
+        }
+        return count;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println(contarVogais("hello world")); // 3
     }
 }
