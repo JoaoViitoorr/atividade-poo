12bf5ee45add59dcfa083b419f1b6dad8bfe0121
JoaoViitoorr <joaovitoroliveirasimao9@gmail.com>
2025-04-06 19:15:55 -0300

    quarto commit bracnh04

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
+    public static String inverterString(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println(inverterString("hello")); // "olleh"
     }
 }
