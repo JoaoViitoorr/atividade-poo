1b27b54c10f8a0f268c00d7f82de4263bbb1e91c
JoaoViitoorr <joaovitoroliveirasimao9@gmail.com>
2025-04-06 19:12:43 -0300

    terceiro commit branch 03

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
 public class Main {
+    public static int fatorial(int n) {
+        int fatorial = 1;
+        for (int i = 1; i <= n; i++) {
+            fatorial *= i;
+        }
+        return fatorial;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println(fatorial(5)); // 120
     }
 }
